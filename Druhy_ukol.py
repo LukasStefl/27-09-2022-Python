42e0154eebea52cf4bde7d66ae9b7b46a98a9082
LukasStefl <lukas.stefl@student.ossp.cz>
2022-09-27 10:15:08 +0200

    commit

--- Druhy_ukol.py
+++ Druhy_ukol.py
@@ -1,2 +1,6 @@
-num = 1456
-print(str(num)[::-1])
+string = "Ramanujan byl velmi zajímavý geniální indický matematik."
+words = string.split()
+
+
+number_of_words = len(words)
+print(number_of_words)
