2dce0af71cc2cb85cc145d75daf147068159fe56
LukasStefl <lukas.stefl@student.ossp.cz>
2022-09-27 15:15:43 +0200

    commit

--- Paty_ukol.py
+++ Paty_ukol.py
@@ -1,1 +1,10 @@
-list = [1,2,3,4,5,6]
+list = ['1','2','3','4','5','6']
+vlozeni = int(input("Vlož číslo: "))
+i = 0
+while i < 7:
+    if vlozeni == list[i]:
+        print("Je stejné.")
+        break;
+    else:
+        print("Není stejné.")
+        break;
---
+++ Sesty_ukol.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3, 4, 5, 6, 7]
+numbers[0] = 1
+numbers[1] = 4
+numbers[2] = 9
+numbers[3] = 16
+numbers[4] = 25
+numbers[5] = 36
+numbers[6] = 49
+
+print(numbers)
