d263ac2ebe2f69097b6b80268d32c2ad70768abe
LukasStefl <lukas.stefl@student.ossp.cz>
2022-09-27 15:23:54 +0200

    commit

--- Paty_ukol.py
+++ Paty_ukol.py
@@ -2,6 +2,6 @@ list = [1,2,3,4,5,6]
 vlozeni = int(input("Vlož číslo: "))
 for i in list:
     if(i == vlozeni):
-        print("Je v poli")
+        print("Je v poli.")
     else:
         print("Není v poli.")
